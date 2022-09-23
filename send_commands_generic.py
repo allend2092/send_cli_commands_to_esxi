1be2d8a77dd6bba369cbb24911f2d96db0f7776c
daryl <daryl.allen.jr@gmail.com>
2022-09-22 20:49:18 -0600

    Added function definition to produce connection object and another to send commands to the cli.

--- send_commands_generic.py
+++ send_commands_generic.py
@@ -11,12 +11,21 @@ def connect(host, username, password):
     except:
         print('Failed to connect. Quitting!')
 
+def send_command(server_connection, cli_command):
+    resp = server_connection.send_command(cli_command)
+    return resp
+
+
+if __name__ == "__main__":
     esxi_host = '172.17.0.56'
-    password = 'VMware1!'
     username = 'root'
+    password = 'VMware1!'
 
     net_connection = connect(esxi_host, username, password)
 
-    response = net_connection.send_command("net-stats -l")
+    response1 = send_command(net_connection, "net-stats -l")
+    response2 = send_command(net_connection, "esxcfg-vmknic -l")
 
-    print(response)
+    print(response1)
+    print()
+    print(response2)
